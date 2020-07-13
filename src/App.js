efbfca1fc6a6cb4edad491688120b604826a09ce
moontech <claudianunes99@hotmail.com>
2020-07-13 11:26:55 +0100

    adad

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React from 'react';
 
-
-
 import { BrowserRouter as Router, Route, Redirect, Switch } from "react-router-dom";
 
 import AuthService from "./view/auth.service";
@@ -90,87 +88,87 @@ class GestorCompetencias extends React.Component {
 
   }
 
-  // async componentDidMount() {
-  //   const response = await api.get("posts");
+  async componentDidMount() {
+    const response = await api.get("posts");
 
-  //   this.setState({
-  //     uploadedFiles: response.data.map(file => ({
-  //       id: file._id,
-  //       name: file.name,
-  //       readableSize: filesize(file.size),
-  //       preview: file.url,
-  //       uploaded: true,
-  //       url: file.url
-  //     }))
-  //   });
-  // }
+    this.setState({
+      uploadedFiles: response.data.map(file => ({
+        id: file._id,
+        name: file.name,
+        readableSize: filesize(file.size),
+        preview: file.url,
+        uploaded: true,
+        url: file.url
+      }))
+    });
+  }
 
-  // handleUpload = files => {
-  //   const uploadedFiles = files.map(file => ({
-  //     file,
-  //     id: uniqueId(),
-  //     name: file.name,
-  //     readableSize: filesize(file.size),
-  //     preview: URL.createObjectURL(file),
-  //     progress: 0,
-  //     uploaded: false,
-  //     error: false,
-  //     url: null
-  //   }));
+  handleUpload = files => {
+    const uploadedFiles = files.map(file => ({
+      file,
+      id: uniqueId(),
+      name: file.name,
+      readableSize: filesize(file.size),
+      preview: URL.createObjectURL(file),
+      progress: 0,
+      uploaded: false,
+      error: false,
+      url: null
+    }));
 
-  //   this.setState({
-  //     uploadedFiles: this.state.uploadedFiles.concat(uploadedFiles)
-  //   });
+    this.setState({
+      uploadedFiles: this.state.uploadedFiles.concat(uploadedFiles)
+    });
 
-  //   uploadedFiles.forEach(this.processUpload);
-  // };
+    uploadedFiles.forEach(this.processUpload);
+  };
 
-  // updateFile = (id, data) => {
-  //   this.setState({
-  //     uploadedFiles: this.state.uploadedFiles.map(uploadedFile => {
-  //       return id === uploadedFile.id
-  //         ? { ...uploadedFile, ...data }
-  //         : uploadedFile;
-  //     })
-  //   });
-  // };
+  updateFile = (id, data) => {
+    this.setState({
+      uploadedFiles: this.state.uploadedFiles.map(uploadedFile => {
+        return id === uploadedFile.id
+          ? { ...uploadedFile, ...data }
+          : uploadedFile;
+      })
+    });
+  };
 
-  // processUpload = uploadedFile => {
-  //   const data = new FormData();
+  processUpload = uploadedFile => {
+    const data = new FormData();
 
-  //   data.append("file", uploadedFile.file, uploadedFile.name);
+    data.append("file", uploadedFile.file, uploadedFile.name);
 
-  //   api
-  //     .post("posts", data, {
-  //       onUploadProgress: e => {
-  //         const progress = parseInt(Math.round((e.loaded * 100) / e.total));
+    api
+      .post("posts", data, {
+        onUploadProgress: e => {
+          const progress = parseInt(Math.round((e.loaded * 100) / e.total));
 
-  //         this.updateFile(uploadedFile.id, {
-  //           progress
-  //         });
-  //       }
-  //     })
-  //     .then(response => {
-  //       this.updateFile(uploadedFile.id, {
-  //         uploaded: true,
-  //         id: response.data._id,
-  //         url: response.data.url
-  //       });
-  //     })
-  //     .catch(() => {
-  //       this.updateFile(uploadedFile.id, {
-  //         error: true
-  //       });
-  //     });
-  // };
+          this.updateFile(uploadedFile.id, {
+            progress
+          });
+        }
+      })
+      .then(response => {
+        this.updateFile(uploadedFile.id, {
+          uploaded: true,
+          id: response.data._id,
+          url: response.data.url
+        });
+      })
+      .catch(() => {
+        this.updateFile(uploadedFile.id, {
+          error: true
+        });
+      });
+  };
 
-  // handleDelete = async id => {
-  //   await api.delete(`posts/${id}`);
+  handleDelete = async id => {
+    await api.delete(`posts/${id}`);
 
-  //   this.setState({
-  //     uploadedFiles: this.state.uploadedFiles.filter(file => file.id !== id)
-  //   });
-  // };
+    this.setState({
+      uploadedFiles: this.state.uploadedFiles.filter(file => file.id !== id)
+    });
+  };
 
   componentWillUnmount() {
     this.state.uploadedFiles.forEach(file => URL.revokeObjectURL(file.preview));
@@ -193,7 +191,7 @@ class GestorCompetencias extends React.Component {
             <PrivateRoute path="/DEVPesquisarProjetos" exact component={DEVPesquisarProjetos} roles={[Role.DEV, Role.TL]} />
 
             <PrivateRoute path="/RHAdicionarCompetencias" exact component={RHAdicionarCompetencias} roles={[Role.RH]} />
-            <PrivateRoute onUpload={this.handleUpload} path="/RHAdicionarDev" exact component={RHAdicionarDev} roles={[Role.RH]} />
+            <PrivateRoute path="/RHAdicionarDev" exact component={RHAdicionarDev} roles={[Role.RH]} />
             <PrivateRoute path="/RHatribuirPapeisTL" exact component={RHatribuirPapeisTL} roles={[Role.RH]} />
             <PrivateRoute path="/RHCriarProjetos" exact component={RHCriarProjetos} roles={[Role.RH]} />
             <PrivateRoute path="/RHDashboard" exact component={RHDashboard} roles={[Role.RH]} />
